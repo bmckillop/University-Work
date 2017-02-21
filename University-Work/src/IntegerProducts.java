82fd832e5a872f057c791e787f471aa7e93adf38
bmckillop <b.mckillop@kainos.com>
2017-02-21 15:10:14 +0000

    week4

---
+++ University-Work/src/IntegerProducts.java
@@ -0,0 +1,18 @@
+/**
+ * Created by benm on 21/02/2017.
+ */
+public class IntegerProducts {
+
+    public static void main (String [] args) {
+
+        int a = 15;
+        int b = 45;
+
+        int answer = a * b;
+
+        System.out.println("The answer of the two hard coded values is " + answer);
+
+
+    }
+
+}
---
+++ University-Work/src/PerimeterCalculator.java
@@ -0,0 +1,18 @@
+/**
+ * Created by benm on 21/02/2017.
+ */
+public class PerimeterCalculator {
+
+    public static void main (String [] args){
+
+        double length, height, area;
+
+        length = 15;
+        height = 25;
+
+        area = (length * 2) + (height *2);
+
+        System.out.println("The area of the rectangle is " + area);
+
+    }
+}
