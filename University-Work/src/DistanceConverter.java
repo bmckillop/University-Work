2b3976d07897a70dbd520879cc7c88b47cee676c
bmckillop <b.mckillop@kainos.com>
2017-02-21 10:57:33 +0000

    -a

---
+++ University-Work/src/DistanceConverter.java
@@ -0,0 +1,16 @@
+/**
+ * Created by benm on 21/02/2017.
+ */
+public class DistanceConverter {
+
+    public static void main (String [] args){
+
+        double kilometers, miles;
+
+        kilometers = 12.5;
+        miles = kilometers / 1.609;
+
+        System.out.println("miles = " + miles);
+    }
+
+}
